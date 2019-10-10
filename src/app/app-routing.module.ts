d4750eca5b1c86baf2c6400e3c5355ac119b26aa
Suryansh <suryanshsrivastava9616@gmail.com>
2019-10-10 23:03:01 +0530

    Intial Commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AddProductComponent } from './add-product/add-product.component';
+import { ListProductComponent } from './list-product/list-product.component';
+import { HistoryComponent } from './history/history.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path:'', component: HomeComponent},
+  { path:'add-product', component:AddProductComponent},
+  { path:'list-product', component:ListProductComponent},
+  { path:'history', component:HistoryComponent},
+  { path:'**', component:PageNotFoundComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AddProductComponent } from './add-product/add-product.component';
+import { ListProductComponent } from './list-product/list-product.component';
+import { HistoryComponent } from './history/history.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddProductComponent,
+    ListProductComponent,
+    HistoryComponent,
+    PageNotFoundComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
